3f671514e799c1b0c14db28eb238ec5e83fbe7ba
FilinMen <fiilinkadabra@tutamail.com>
2025-05-29 15:52:27 +0300

    efwf

---
+++ parallel.py
@@ -0,0 +1,23 @@
+import threading
+import time
+
+progress = 0
+
+def monitor():
+    global progress
+    progress += 1
+    print(f"прогресс: {progress}/50")
+    
+def sleep():
+    time.sleep(1)
+
+
+for i in range(50):
+    threads = threading.Thread(target=monitor, name="Thread-1")
+    threads1 = threading.Thread(target=sleep, name="Thread-2")
+    threads.start()
+    threads1.start()
+    threads.join()
+    threads1.join()
+ 
+
